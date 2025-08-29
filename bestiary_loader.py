6cb78a6f7fc0cc84dd3bd8f533e29183659d40b0
Kathryn <kathryn@mckaysciences.com>
2025-08-29 15:22:30 -0400

    Now merges the beast lists

--- bestiary_loader.py
+++ bestiary_loader.py
@@ -12,8 +12,10 @@ for path in ['datasworn/classic/classic.json', 'datasworn/delve/delve.json']:
         npc_list = json.load(f).get('npcs', [])
     for nature_key, nature_properties in npc_list.items():
         beast_collection = { beast_key.replace('_', '-'): beast_properties for beast_key, beast_properties in nature_properties.get('contents', {}).items() }
+        beasts_just_names = { beast_key: beast_properties.get('name') for beast_key, beast_properties in beast_collection.items() }
         NATURES.setdefault(nature_key, {
             'name': nature_properties.get('name')
-            , 'beasts': { beast_key: beast_properties.get('name') for beast_key, beast_properties in beast_collection.items() }
+            , 'beasts': {}
             })
+        NATURES[nature_key]['beasts'].update(beasts_just_names)
         BESTIARY_DATA.update({beast_name: {k: v for k, v in beast_properties.items() if k in BEAST_DATA_KEYS} for beast_name, beast_properties in beast_collection.items()})
